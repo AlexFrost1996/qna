80f0211f782a190c68efed02638658fb9700f86e
Alex <pentagon-1996@tut.by>
2021-12-14 22:48:38 +0300

    Devise with sign in spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'questions#index'
+  
+  devise_for :users
+
   resources :questions, shallow: true do
     resources :answers, shallow: true, except: :index
   end
