b900065d036db12adff7ff8cd55d869275213af3
Alex <pentagon-1996@tut.by>
2022-01-05 21:37:01 +0300

    add delete of attached files

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -66,6 +66,21 @@ feature 'User can edit his question' do
           expect(page).to have_link 'spec_helper.rb'
         end
       end
+      
+      scenario 'delete attachments' do
+        within '.question' do
+          first('.attachment').click_on 'Delete file'
+          expect(page).to_not have_link 'rails_helper.rb'
+          expect(page).to have_link 'spec_helper.rb'
+        end
+      end
+
+      scenario "can't delete other user's attachments" do
+        visit question_path(other_question)
+        within '.question' do
+          expect(page).to_not have_link 'Delete file'
+        end
+      end
     end
   end
 
