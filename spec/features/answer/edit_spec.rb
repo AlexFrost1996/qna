43b5567f3dd6f9db8862883da70e8260fb2619e4
AlexFrost1996 <79652667+AlexFrost1996@users.noreply.github.com>
2022-01-12 22:57:00 +0300

    Revert "add file attachments when editing question and answer"
    This reverts commit eb21cfd447fa5a30a17ad2a53077294823411cfa.

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -2,21 +2,23 @@ require 'rails_helper'
 
 feature 'user can edit his answer' do
   given!(:user) { create(:user) }
-  given!(:other_user) { create(:user) }
   given!(:question) { create(:question, user: user) }
-  given!(:other_question) { create(:question, user: other_user) }
   given!(:answer) { create(:answer, user: user, question: question) }
-  given!(:other_answer) { create(:answer, user: other_user, question: other_question) }
+  given!(:other_user) { create(:user) }
+
+  scenario 'Unauthenticated can not edit answer' do
+    visit question_path(question)
+
+    expect(page).to_not have_link "Edit"
+  end
 
   describe 'Authenticated user', js: true do
-    background do
-      sign_in user
+    scenario 'edits his answer' do
+      sign_in(user)
       visit question_path(question)
-    end
 
-    scenario 'can edit his answer' do
       within '.answers' do
-        click_on "Edit"
+        click_on 'Edit'
         fill_in 'Your answer', with: 'edited answer'
         click_on 'Save'
 
@@ -26,7 +28,17 @@ feature 'user can edit his answer' do
       end
     end
 
+    scenario 'can not edit other answer' do
+      sign_in(other_user)
+      visit question_path(question)
+
+      expect(page).to_not have_link "Edit"
+    end
+
     scenario 'edit his answer with errors' do
+      sign_in(user)
+      visit question_path(question)
+      
       within '.answers' do
         click_on "Edit"
         fill_in 'Your answer', with: ''
@@ -34,35 +46,5 @@ feature 'user can edit his answer' do
         expect(page).to have_content "Body can't be blank"
       end
     end
-
-    scenario 'Other user can not edit answer' do
-      visit question_path(other_question)
-      within '.answers' do
-        expect(page).to_not have_link "Edit"
-      end
-    end
-
-    context 'attachments' do
-      background do
-        within '.answers' do
-          click_on "Edit"
-          attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
-          click_on 'Save'
-        end
-      end
-      
-      scenario 'add attachments' do
-        within '.answers' do
-          expect(page).to have_link 'rails_helper.rb'
-          expect(page).to have_link 'spec_helper.rb'
-        end
-      end
-    end
-  end
-
-  scenario 'Unauthenticated can not edit answer' do
-    visit question_path(question)
-
-    expect(page).to_not have_link "Edit"
   end
 end
